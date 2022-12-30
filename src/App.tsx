359a432755206c85974c34ceb3bbb58e0e3f2017
yermie <yermolovpersonal@gmail.com>
2022-12-30 01:02:52 -0800

    More layout work, added an svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
+import Wheel from './components/Wheel';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h2 className='headings'>Definitely Good Random Item Picker</h2>
+        <h3 className='headings'>Absolutely not rigged or Anything</h3>
+        <h4 className='headings' id='lastHeading'>Works perfectly fine.</h4>
+        <Wheel />
+        <h4 className='label'>Each New Line is Separate Input</h4>
+        <textarea>
+          Input Box
+        </textarea>
+        <h4 className='label'>Ok, you caught me. Select this box to make it fair.</h4>
+        <input type="checkbox" />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Wheel.tsx
@@ -0,0 +1,16 @@
+
+
+const Wheel = () => {
+    return(
+        <div>
+            <svg>
+                <circle />
+                <polygon points="500,275 500,225 415,250"/>
+            </svg>
+            <br></br>
+            <button>Go</button>
+        </div>
+    )
+}
+
+export default Wheel;
